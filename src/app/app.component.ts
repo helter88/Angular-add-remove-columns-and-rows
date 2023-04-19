5dd90121a5e6126fcd3f6769b3e0b301676f6fd1
artur-helta <artur.helta@onet.pl>
2023-04-19 13:10:04 +0200

    display values in table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'spreadsheet';
+
+  form = this.fb.group({
+    name: 'Artur',
+    displayedHead: this.fb.array([1,2]),
+    myFormArray: this.fb.array([
+      this.fb.array(['ala','lolo']),
+      this.fb.array(['kotek','koko']),
+
+    ])
+  })
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  get displayedHead(): FormArray {
+    return this.form.get("displayedHead") as FormArray;
+  }
+
+  get myFormArray(): FormArray {
+    return this.form.get("myFormArray") as FormArray;
+  }
+  
+   myFormArray2(i: number): FormArray {
+   let nestedArray = this.form.get("myFormArray") as FormArray<FormArray>;
+   let b =  nestedArray.controls[i];
+    return b;
+  }
+
+  displalog(col:any) {
+    console.log(col)
+  }
+  onSave() {
+    console.log(this.form.value)
+  }
 }
